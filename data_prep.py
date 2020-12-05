fbb5a01aa59d252dedc1c8a94bb42753ff370273
akshay-raj-dhamija <akshay.raj.dhamija@gmail.com>
2020-12-04 19:56:26 -0700

    bug fix for h5py version

--- data_prep.py
+++ data_prep.py
@@ -18,9 +18,9 @@ def read_features(args, feature_file_names=None, cls_to_process=None):
             temp = []
             for hf, layer_name in file_layer_comb:
                 temp.append(torch.squeeze(torch.tensor(hf[cls][layer_name])))
-            image_names= hf[cls]['image_names'][()].tolist()
+            image_names = hf[cls]['image_names'][()].tolist()
             features = torch.cat(temp,dim=1)
-            yield cls,features,image_names
+            yield cls, features, image_names
     finally:
         for h in h5_objs:
             h.close()
@@ -32,7 +32,7 @@ def prep_single_chunk(args, cls_to_process):
     data_to_return = {}
     for cls, feature, image_names in features_gen:
         data_to_return[cls] = {}
-        data_to_return[cls]['images'] = np.array([f"{cls}/{_}" for _ in image_names], dtype='<U30')
+        data_to_return[cls]['images'] = np.array([f"{cls}/{_.decode('ascii')}" for _ in image_names], dtype='<U30')
         data_to_return[cls]['features'] = feature
     return data_to_return
 
@@ -46,7 +46,7 @@ def prep_all_features_parallel(args, all_class_names=None):
         all_class_names = all_class_names[:100]
         cls_per_chunk = 2
     all_class_batches = [all_class_names[i:i+cls_per_chunk] for i in range(0, len(all_class_names), cls_per_chunk)]
-    p = mp.Pool(min(30,mp.cpu_count()))
+    p = mp.Pool(min(30, mp.cpu_count()))
     all_data = p.map(partial(prep_single_chunk, args), all_class_batches)
     all_classes={}
     for data_returned in all_data:
